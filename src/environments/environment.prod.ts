e599f1f2e555fb55e4249c13c1b9a9287ec57f91
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2025-01-06 00:11:39 +0530

    changes in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,6 +10,7 @@ export const environment = {
     appId: "1:63962912478:web:9a5051201340837cfa144f"
   },
   CONTACT_NUMBER: '8886361431',
+  
   emailJS:{
     public_key:'o7zOCvy3iM3N4s3b_',
     service_Id:'service_797v1lm',
