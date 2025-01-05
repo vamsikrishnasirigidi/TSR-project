c10a9eef41f64e3a513da59fa086226c8ce39306
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2025-01-06 00:04:22 +0530

    mail link and new changes

--- src/app/api/services/email/email.service.ts
+++ src/app/api/services/email/email.service.ts
@@ -14,35 +14,35 @@ export class EmailService {
   }
   ngOnInit(): void { emailjs.init(environment.emailJS.public_key);}
 
-  sendEmail(formData: any) {
-    const emailData = {
-      personalizations: [{
-        to: [{ email: 'vamsikrishna.sirigidi@imaginnovate.com' }],
-        from: { email: environment.sendgrid.verified_Email },  
-        subject: 'New Contact Form Submission'
-      }],
-      content: [{
-        type: 'text/html',
-        value: `
-          <div style="padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
-            <h2>Contact Details</h2>
-            <p><strong>Name:</strong> ${formData.name}</p>
-            <p><strong>Email:</strong> ${formData.email}</p>
-            <p><strong>Contact:</strong> ${formData.contactNumber}</p>
-            <p><strong>Address:</strong> ${formData.address}</p>
-            <p><strong>Description:</strong> ${formData.message}</p>
-          </div>
-        `
-      }]
-    };
+  // sendEmail(formData: any) {
+  //   const emailData = {
+  //     personalizations: [{
+  //       to: [{ email: 'vamsikrishna.sirigidi@imaginnovate.com' }],
+  //       from: { email: environment.sendgrid.verified_Email },  
+  //       subject: 'New Contact Form Submission'
+  //     }],
+  //     content: [{
+  //       type: 'text/html',
+  //       value: `
+  //         <div style="padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+  //           <h2>Contact Details</h2>
+  //           <p><strong>Name:</strong> ${formData.name}</p>
+  //           <p><strong>Email:</strong> ${formData.email}</p>
+  //           <p><strong>Contact:</strong> ${formData.contactNumber}</p>
+  //           <p><strong>Address:</strong> ${formData.address}</p>
+  //           <p><strong>Description:</strong> ${formData.message}</p>
+  //         </div>
+  //       `
+  //     }]
+  //   };
 
-    return this.http.post(this.corsProxy + this.apiUrl, emailData, {
-      headers: {
-        'Authorization': `Bearer ${environment.sendgrid.apiKey}`,
-        'Content-Type': 'application/json'
-      }
-    });
-  }
+  //   return this.http.post(this.corsProxy + this.apiUrl, emailData, {
+  //     headers: {
+  //       'Authorization': `Bearer ${environment.sendgrid.apiKey}`,
+  //       'Content-Type': 'application/json'
+  //     }
+  //   });
+  // }
   sendEmailToEmailJS(formData: any){
     const templateParams = {
       name: formData.name,
