6a27f05d5f9630cc2175b4b811193936c44b7df6
sirigidivamsikrishna <vamsikrishna.sirigidi@imaginnovate.com>
2025-01-12 21:43:05 +0530

    gallery animation bug fixed

--- src/app/shared/components/gallery/gallery.component.ts
+++ src/app/shared/components/gallery/gallery.component.ts
@@ -7,10 +7,22 @@ import { serverTimestamp } from 'firebase/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LocalServiceService } from 'src/app/api/services/localStorage/local-service.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class GalleryComponent {
   headerText: string =
